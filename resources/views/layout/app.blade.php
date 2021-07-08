816111b0581164c9babaa8d089c64bc60318ff01
AndreiBurbulia <77996991+AndreiBurbulia@users.noreply.github.com>
2021-07-08 16:12:12 +0200

    Aggiunte pagine e relativi collegamenti

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -16,15 +16,15 @@
            <nav>
                <a href="{{route('home')}}">Home</a>
                <a href="{{route('characters')}}">Characters</a>
-               <a href="">Comics</a>
-               <a href="">Movies</a>
-               <a href="">TV</a>
-               <a href="">Games</a>
-               <a href="">Collectibles</a>
-               <a href="">Videos</a>
-               <a href="">Fans</a>
-               <a href="">News</a>
-               <a href="">Shop</a>
+               <a href="{{route('comics')}}">Comics</a>
+               <a href="{{route('movies')}}">Movies</a>
+               <a href="{{route('tv')}}">TV</a>
+               <a href="{{route('games')}}">Games</a>
+               <a href="{{route('collectibles')}}">Collectibles</a>
+               <a href="{{route('videos')}}">Videos</a>
+               <a href="{{route('fans')}}">Fans</a>
+               <a href="{{route('news')}}">News</a>
+               <a href="{{route('shop')}}">Shop</a>
            </nav>
        </header>
 
